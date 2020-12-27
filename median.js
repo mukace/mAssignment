935fac701a781d3bc67ffee167e5ca03634e9822
Mukesh Thakur <mukacet@gmail.com>
2020-12-27 20:40:05 +0530

    moneytap assignments

---
+++ median.js
@@ -0,0 +1,14 @@
+const median = (a1, a2) => {
+    let x = a1.concat(a2);
+    x.sort(function (a,b) {
+      return a - b;
+    });
+    let len = x.length;
+    
+    return len%2 === 0 ? (x[Math.floor(len/2)-1] + x[Math.ceil(len/2)])/2 : x[Math.floor(len/2)];
+  
+  }
+  
+  let a = [0,2,3,5,9];
+  let b = [1,4];
+  console.log(median(a,b));
